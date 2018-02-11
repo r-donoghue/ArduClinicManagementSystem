a4dd83b6512975e578c6d4f58f2ec1bd3cd9bea9
Ryan Donoghue <ryandonoghue26@gmail.com>
2018-02-11 17:28:00 +0000

    Fallback Commit)

--- web/router.ex
+++ web/router.ex
@@ -16,7 +16,7 @@ defmodule Cmsv1.Router do
   scope "/", Cmsv1 do
     pipe_through :browser # Use the default browser stack
 
-    get "/", PageController, :index
+    get "/", PatientController, :index
     resources "/pharms", PharmacyController
     resources "/gps", GPController
     resources "/cdoctors", CDoctorController
