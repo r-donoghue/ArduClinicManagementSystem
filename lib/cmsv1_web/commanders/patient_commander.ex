533c644f8efa105a75539fdd7315420a0ee3fdfc
Ryan Donoghue <ryandonoghue26@gmail.com>
2018-02-26 22:38:10 +0000

    Fixed commander error

--- lib/cmsv1_web/commanders/patient_commander.ex
+++ lib/cmsv1_web/commanders/patient_commander.ex
@@ -6,7 +6,7 @@ defmodule Cmsv1.PatientCommander do
 
   def refresh_pharms(socket, _sender) do
     pharm = Repo.all(Pharmacy) |> Enum.map(&{&1.name, &1.pharm_id}) |> Enum.into(%{})  
-    IO.inspect pharmacy
+    IO.inspect pharm
     set_prop socket, "#patient_pharm_id" , pharms: pharm
   end
 end
